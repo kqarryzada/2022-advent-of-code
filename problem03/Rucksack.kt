03c433a448f3a0a7c1124fe6c0e975febd5b7e63
kqarryzada <khalid.qarryzada@gmail.com>
2022-12-22 15:04:22 -0600

    Add a solution for problem 3.

---
+++ problem03/Rucksack.kt
@@ -0,0 +1,40 @@
+package problem03
+
+import java.io.File
+import java.lang.RuntimeException
+
+private const val INPUT_FILE = "input.txt"
+
+private fun getPriority(char: Char): Int {
+    val lowerChar = char.lowercaseChar()
+    if (lowerChar < 'a' || lowerChar > 'z') {
+        throw IllegalArgumentException("Invalid character requested.")
+    }
+
+    var priority = lowerChar - 'a' + 1
+    if (char.isUpperCase()) { priority += 26 }
+
+    return priority
+}
+
+fun main() {
+    var prioritySum = 0L
+    File(INPUT_FILE).forEachLine { sack ->
+        val firstCompartment = sack.substring(0, sack.length / 2)
+        val secondCompartment = sack.substring(sack.length / 2)
+
+        var found = false
+        for (letter:Char in firstCompartment) {
+            if (secondCompartment.contains(letter)) {
+                prioritySum += getPriority(letter)
+                found = true
+                break
+            }
+        }
+        if (!found) {
+            throw RuntimeException("Unable to find an element that appears twice in '$sack'.")
+        }
+    }
+
+    println("The sum of the priorities is: $prioritySum")
+}
